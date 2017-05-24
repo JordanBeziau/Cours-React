25c94a55a2b82e94d57fcffe31b93f87d3fcd1cc
JordanBeziau <jordanbeziau@gmail.com>
2017-05-24 19:27:25 +0200

    add setState text

--- Projet-1-Markdown/markdown/src/index.js
+++ Projet-1-Markdown/markdown/src/index.js
@@ -12,18 +12,28 @@ class App extends React.Component {
     text: sampleText
   };
 
+  changeText = (event) => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row">
 
           <div className="col-xs-6">
-            <textarea rows="35" className="form-control" value={this.state.text}>
-            </textarea>
+            <textarea
+              rows="35"
+              className="form-control"
+              value={this.state.text}
+              onChange={(event) => this.changeText(event)}
+            ></textarea>
           </div>
 
           <div className="col-xs-6">
             <h1>Résultat :</h1>
+            <p>{this.state.text}</p>
           </div>
 
         </div>
