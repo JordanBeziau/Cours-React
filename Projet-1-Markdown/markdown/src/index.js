9384bd4ac0c9859ad1d3626168c2dcef19dae78e
JordanBeziau <jordanbeziau@gmail.com>
2017-05-24 18:28:03 +0200

    Basic imports and app structures

--- Projet-1-Markdown/markdown/src/index.js
+++ Projet-1-Markdown/markdown/src/index.js
@@ -1,1 +1,30 @@
-// Ici on code notre App
+import React from 'react';
+import { render } from 'react-dom';
+// CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+
+          <div className="col-xs-6">
+            <textarea rows="35" className="form-control"></textarea>
+          </div>
+
+          <div className="col-xs-6">
+            <h1>Résultat :</h1>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('root')
+);
