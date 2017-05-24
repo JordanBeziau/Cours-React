1a033387a04e45358176c1c8af4528c78fe0f018
JordanBeziau <jordanbeziau@gmail.com>
2017-05-24 19:06:13 +0200

    create first state...

--- Projet-1-Markdown/markdown/src/index.js
+++ Projet-1-Markdown/markdown/src/index.js
@@ -3,15 +3,23 @@ import { render } from 'react-dom';
 // CSS
 import './style/css/bootstrap.min.css';
 import './index.css';
+// JS
+import { sampleText } from './sampleText';
 
 class App extends React.Component {
+
+  state = {
+    text: sampleText
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row">
 
           <div className="col-xs-6">
-            <textarea rows="35" className="form-control"></textarea>
+            <textarea rows="35" className="form-control" value={this.state.text}>
+            </textarea>
           </div>
 
           <div className="col-xs-6">
