c5c9d3d4e0984b838ce82dd072284141db67329f
JordanBeziau <jordanbeziau@gmail.com>
2017-06-01 20:36:35 +0200

    Mise en place des components

---
+++ Projet 2 chatBox/chatBox/src/components/Message.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Message extends React.Component {
+  render() {
+    return (
+      <div className="message">
+        <p className="user-message">
+          Hello World !
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Message;
--- Projet 2 chatBox/chatBox/src/index.js
+++ Projet 2 chatBox/chatBox/src/index.js
@@ -1,1 +1,12 @@
-// Code React.js
+import React from 'react';
+import { render } from 'react-dom';
+// Components
+import App from './components/App';
+import Connexion from './components/Connexion';
+// CSS
+import './index.css';
+
+render(
+  <App />,
+  document.getElementById('root')
+);
