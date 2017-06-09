24ac5ca324414278ab7b96627c702c60a44c12a0
JordanBeziau <jordanbeziau@gmail.com>
2017-06-09 09:24:17 +0200

    formulaire

--- Projet 2 chatBox/chatBox/src/components/Formulaire.js
+++ Projet 2 chatBox/chatBox/src/components/Formulaire.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 class Formulaire extends React.Component {
+
+  sendMessage = event => {
+    event.preventDefault();
+    console.log(this.message.value);
+  };
+
   render() {
     return (
       <form className="form"
         onSubmit={e => {this.sendMessage(e)}}
       >
-        <textarea required maxLength="140"></textarea>
+        <textarea required maxLength="140"
+          ref={input => this.message = input}
+        ></textarea>
         <div className="info">140</div>
         <button type="submit">Envoyer</button>
       </form>
