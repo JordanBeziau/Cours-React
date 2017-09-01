1d545258f6e8ec4da3e5c746a4c1c90054922761
JordanBeziau <jordanbeziau@gmail.com>
2017-09-01 20:32:03 +0200

    Add CreateContact component + screen state to App

--- Udada/contactsApp/client/src/App.js
+++ Udada/contactsApp/client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
+    screen: 'create', // list or create
     contacts : [ ]
   }
 
@@ -22,10 +24,15 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts
-          onRemoveContact={this.removeContact}
-          contacts={this.state.contacts}
-        />
+        { this.state.screen === 'list' && (
+          <ListContacts
+            onRemoveContact={this.removeContact}
+            contacts={this.state.contacts}
+          />
+        ) }
+        { this.state.screen === 'create' && (
+          <CreateContact />
+        ) }
       </div>
     )
   }
