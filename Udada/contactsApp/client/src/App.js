6f7a36911cf59fba3621eb8f4258b9dde89ecfff
JordanBeziau <jordanbeziau@gmail.com>
2017-09-01 21:13:36 +0200

    Using BrowserRouter, Link, Route from react-router to manage route

--- Udada/contactsApp/client/src/App.js
+++ Udada/contactsApp/client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
-    screen: 'create', // list or create
+    screen: 'list', // list or create
     contacts : [ ]
   }
 
@@ -24,15 +25,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        { this.state.screen === 'list' && (
+        <Route exact path="/" render={ () => (
           <ListContacts
             onRemoveContact={this.removeContact}
             contacts={this.state.contacts}
+            onNavigate={ () => this.setState({ screen: 'create' }) }
           />
-        ) }
-        { this.state.screen === 'create' && (
-          <CreateContact />
-        ) }
+        ) } />
+        <Route path="/create" component={ CreateContact } />
       </div>
     )
   }
